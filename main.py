5c1c5e4cc7afabc97a6151f9432bd45f673bc583
salnyed <agorbuncova2004@gmail.com>
2023-12-21 12:09:13 +0400

    Added window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("acdad")
+import sys
+
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
